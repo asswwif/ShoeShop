975a1aeade28fed5d44d86c15385225a959ed19f
asswwif <yerofeyevam@gmail.com>
2025-10-12 16:04:48 +0300

    Виправлено помилку, додано обробник label2

--- WindowsFormsApp2/SignInForm.cs
+++ WindowsFormsApp2/SignInForm.cs
@@ -18,6 +18,7 @@ namespace WindowsFormsApp2
             InitializeComponent();
         }
 
+        private void label2_Click(object sender, EventArgs e) { }
 
         private void label3_Click(object sender, EventArgs e) { }
 
