6318e8ceb554cb29d72a0db3bb5118c5f8453fce
asswwif <yerofeyevam@gmail.com>
2025-10-14 18:18:12 +0300

    зміни в дизайні

--- WindowsFormsApp2/Products.Designer.cs
+++ WindowsFormsApp2/Products.Designer.cs
@@ -194,6 +194,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(42, 166);
             this.dataGridView1.Name = "dataGridView1";
--- WindowsFormsApp2/SalesConfirmationForm.Designer.cs
+++ WindowsFormsApp2/SalesConfirmationForm.Designer.cs
@@ -181,6 +181,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(79, 61);
             this.dataGridView1.Name = "dataGridView1";
